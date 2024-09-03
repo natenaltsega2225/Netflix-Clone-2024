12b8518af635c547b67d43b163cf76ec93f7cf36
Natnael Tsega <42552538+natenaltsega2225@users.noreply.github.com>
2024-09-03 15:44:21 -0500

    header and footer

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import Home from '../src/pages/Home/Home'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <h1>Netflix</h1>
-    </>
+    <div className="App">
+     <Home/>
+    </div>
   )
 }
 
