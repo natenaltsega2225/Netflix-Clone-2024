f35a51dca24687acef8d3212c87cbbfa65fe087f
Natnael Tsega <42552538+natenaltsega2225@users.noreply.github.com>
2024-10-21 18:33:38 -0500

    attempting deployment

--- netflix-clone/src/Components/Header/Header.jsx
+++ netflix-clone/src/Components/Header/Header.jsx
@@ -4,7 +4,7 @@ import SearchIcon from "@mui/icons-material/Search";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
-import '/Header.css';
+import './Header.css';
 
 const Header = () => {
   const [show, setShow] = useState(false);
